d47c7392225d9024dc05590701f9cc60ae7725b3
xidaokun <xidaokun@elastos.org>
2019-05-28 17:45:46 +0800

    add capsule download

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentExplore.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentExplore.java
@@ -1,12 +1,20 @@
 package com.breadwallet.presenter.fragments;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,8 +25,11 @@ import com.breadwallet.tools.adapter.ExploreAppsAdapter;
 import com.breadwallet.tools.animation.SimpleItemTouchHelperCallback;
 import com.breadwallet.tools.listeners.OnStartDragListener;
 import com.breadwallet.tools.manager.BRSharedPrefs;
+import com.breadwallet.tools.util.StringUtil;
 
 public class FragmentExplore extends Fragment implements OnStartDragListener {
+
+    private static final String TAG = FragmentExplore.class.getSimpleName()+"_log";
 
     public static FragmentExplore newInstance(String text) {
 
@@ -81,4 +92,48 @@ public class FragmentExplore extends Fragment implements OnStartDragListener {
     public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
         mItemTouchHelper.startDrag(viewHolder);
     }
+
+    private long downloadCapsule(String url){
+        Log.d(TAG, "capsule url:"+url);
+        if(StringUtil.isNullOrEmpty(url)) return -1;
+        DownloadManager.Request request;
+        try{
+            Uri uri = Uri.parse(url);
+            String fileName = uri.getLastPathSegment();
+            request = new DownloadManager.Request(uri);
+            request.setDestinationInExternalFilesDir(getContext(), Environment.DIRECTORY_DOWNLOADS, fileName+".zip");
+            DownloadManager manager = (DownloadManager) getContext().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
+            long downloadId =  manager.enqueue(request);
+            Uri downloadUri = manager.getUriForDownloadedFile(downloadId);
+            Log.d(TAG, "cache capsule path:"+downloadUri.getPath());
+            registerDownloadReceiver(downloadId, downloadUri);
+            return downloadId;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    private void registerDownloadReceiver(final long downloadId, final Uri uri){
+        IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                Log.d(TAG, "downloadId:"+downloadId);
+                Log.d(TAG, "uri:"+uri.getPath());
+                Log.d(TAG, "id:"+id);
+                if(id == downloadId){
+                    decompression(uri);
+                }
+                context.unregisterReceiver(this);
+            }
+        };
+        getContext().registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    private void decompression(Uri uri){
+
+    }
+
 }
