e08281a251aab635cf95c696bf797cddd3267abe
xidaokun <xidaokun@elastos.org>
2019-06-12 21:22:11 +0800

    备注加密上链

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentExplore.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentExplore.java
@@ -2,6 +2,7 @@ package com.breadwallet.presenter.fragments;
 
 import android.annotation.SuppressLint;
 import android.app.DownloadManager;
+import android.app.Fragment;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,7 +17,6 @@ import android.os.Message;
 import android.security.keystore.UserNotAuthenticatedException;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
