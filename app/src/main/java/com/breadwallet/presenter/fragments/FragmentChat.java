edb461fa1474c378356fe6e59b8638ab0e1e9b70
xidaokun <xidaokun@elastos.org>
2019-12-04 17:43:30 +0800

    add strings

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentChat.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentChat.java
@@ -2,7 +2,9 @@ package com.breadwallet.presenter.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,9 @@ import com.breadwallet.R;
 
 public class FragmentChat extends Fragment {
     private static final String TAG = FragmentExplore.class.getSimpleName() + "_log";
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     public static FragmentChat newInstance(String text) {
         FragmentChat f = new FragmentChat();
@@ -24,6 +29,16 @@ public class FragmentChat extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+        initView(rootView);
         return rootView;
     }
+
+    private void initView(View view) {
+        mTabLayout = view.findViewById(R.id.tab_layout);
+        mViewPager = view.findViewById(R.id.viewpager);
+
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+
 }
